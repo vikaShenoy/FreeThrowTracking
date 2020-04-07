dffcebb02f7e79e089c495eaf87453c945ca684d
vsh33 <vsh33@uclive.ac.nz>
2020-04-07 15:13:27 +1200

    Calculate launch angle and throwing velocity

--- HoughTracker.py
+++ HoughTracker.py
@@ -3,42 +3,41 @@ import numpy
 import copy
 import math
 
+BOX_PADDING = -3
+RADIUS = 20
+
 
 def ball_peak(shot_data):
     """Find the (x, y) co-ordinates for the ball at its peak in the trajectory."""
-    (x, y) = min(shot_data, key=lambda x: x[1])
-    return (int(x), int(y))
+    return min(shot_data, key=lambda x: x[1])
 
 
 def ball_release(shot_data):
     """Find the (x, y) co-ordinates for the ball when its released by the player."""
     # X is a max, y is a min
-    (x, y) = max(shot_data, key=lambda x: x[0] - x[1])
-    return (int(x), int(y))
+    return max(shot_data, key=lambda x: x[0] - x[1])
 
 
 def ball_contact(shot_data, xpeak):
     """Find the (x, y) co-ordinates for the ball when it hits the backboard/hoop for the first time."""
     # Alg: want the co-ords where x is a min and y is a min. Find the minimum of the x * y.
     (xmin, ymax) = min(shot_data, key=lambda x: x[0] + x[1])
-    height = int(ymax)
+    height = ymax
 
     result = ()
     xmax = -10000
     tolerance = 20
 
-    for pos in shot_data:
-        x, y = int(pos[0]), int(pos[1])
+    for (x, y) in shot_data:
         if (height - tolerance < y < height + tolerance) and x > xmax and x < xpeak:
             xmax = x
-            print(1)
             result = (x, y)
 
     return result
 
 
 def detect_ball(frame):
-    """Take in a frame from a video. Detect the basketball in the image and 
+    """Take in a frame from a video. Detect the basketball in the image and
     return a bounding box around its location which can be used for tracking."""
     blur = cv2.GaussianBlur(frame, (9, 9), 0)
     gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
@@ -51,17 +50,55 @@ def detect_ball(frame):
         circles = circles[0]
         # Taking the smallest circle here.
         (x, y, r) = min(circles, key=lambda x: math.pi * (x[2]**2))
+        RADIUS = r
         # Add some tolerance around the ball
-        c = 5
-        widthHeight = (2*r) + 5
+        widthHeight = (2*r) + BOX_PADDING
         return (x-r, y-r, widthHeight, widthHeight)
     else:
         print("No circles found.")
         return
 
 
-def find_launch_angle():
-    """TODO"""
+def find_launch_angle(shot_data, p1, frame, n=3):
+    """Take in shot data and return the angle at which the ball was released by the shooter."""
+    # TODO - implement with a more sophisticated approach (if one exists)
+
+    p2 = ()
+    # Find a frame slightly after the release frame to calculate the launch angle with
+    for i, (x, y) in enumerate(shot_data):
+        if (x, y) == p1:
+            p2 = shot_data[i + n]
+
+    xdiff = p1[0] - p2[0]
+    ydiff = p1[1] - p2[1]
+
+    # Testing
+    # cv2.circle(frame, p1, RADIUS, (255, 0, 0), 2)
+    # cv2.circle(frame, p2, RADIUS, (255, 0, 0), 2)
+    # cv2.imshow("Angle finding", frame)
+    # cv2.waitKey(0)
+    return math.degrees(math.atan(ydiff / xdiff))
+
+
+def find_throw_time(shot_data, release, contact, frame_rate):
+    """Find how long it takes the ball to go from release to contact."""
+    p1 = 0
+    p2 = 0
+
+    for i, pos in enumerate(shot_data):
+        if pos == release:
+            p1 = i
+        elif pos == contact:
+            p2 = i
+
+    frame_diff = p2 - p1
+    return frame_diff / frame_rate
+
+
+def find_launch_velocity(throw_time, release_angle):
+    """."""
+    c = 4.9
+    return c * (throw_time / math.sin(release_angle))
 
 
 def track_ball():
@@ -69,6 +106,7 @@ def track_ball():
     # TODO - handle the case where camera is on the other side of the shooter
     # CONSTRAINT: camera needs to be perpendicular to shot
     cap = cv2.VideoCapture("./FreeThrow.mp4")
+    frame_rate = cap.get(cv2.CAP_PROP_FPS)
     ok, initial_frame = cap.read()
     if not ok:
         print("Error reading inital frame")
@@ -89,7 +127,8 @@ def track_ball():
         ok, bbox = tracker.update(frame)
         fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
         if ok:
-            shot_data.append((bbox[0] + (bbox[2]/2), bbox[1] + (bbox[2]/2)))
+            shot_data.append(
+                (int(bbox[0] + (bbox[2]/2)), int(bbox[1] + (bbox[2]/2))))
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
             cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
@@ -98,12 +137,25 @@ def track_ball():
                     cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
         cv2.imshow("Tracking", frame)
 
-    # TESTING
+    # Extract keypoints
     peak = ball_peak(shot_data)
-    cv2.circle(initial_frame, ball_peak(shot_data), 20, (0, 0, 255), 1)
-    cv2.circle(initial_frame, ball_release(shot_data), 20, (0, 0, 255), 1)
-    cv2.circle(initial_frame, ball_contact(
-        shot_data, peak[0]), 20, (0, 0, 255), 1)
+    release = ball_release(shot_data)
+    contact = ball_contact(shot_data, peak[0])
+
+    # Calculate the launch angle
+    angle = find_launch_angle(
+        shot_data=shot_data, p1=release, frame=initial_frame, n=1)
+    print(f"Angle: {angle}")
+
+    # Calculate the throwing velocity
+    throw_time = find_throw_time(shot_data, release, contact, frame_rate)
+    velocity = find_launch_velocity(throw_time=throw_time, release_angle=angle)
+    print(f"Velocity: {velocity}")
+
+    # TESTING to see positions
+    cv2.circle(initial_frame, peak, RADIUS, (0, 0, 255), 2)
+    cv2.circle(initial_frame, release, RADIUS, (0, 0, 255), 2)
+    cv2.circle(initial_frame, contact, RADIUS, (0, 0, 255), 2)
     cv2.imshow("Ball positions", initial_frame)
     cv2.waitKey(0)
 
